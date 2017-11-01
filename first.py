8e914ded84a356540173c8b708ce4e227d099ec9
zhoufan <zf_sch@126.com>
2017-11-01 19:56:24 +0800

    add sth

--- first.py
+++ first.py
@@ -3,3 +3,4 @@
 print 'hello, python'
 print '2017-6-8 add some message'
 print '2017-6-27 add some message'
+print 'add sth'
---
+++ proxy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git config --global http.proxy 127.0.0.1:8118
+git config --global https.proxy 127.0.0.1:8118
